8c9ace61cf5bcf0a1170d12fe0e09079dc44b495
Psoyeon1117 <128681094+Psoyeon1117@users.noreply.github.com>
2024-02-04 17:33:26 +0900

    Delete 0129/app.js

--- 0129/app.js
+++
@@ -1,4 +0,0 @@
-const container = document.querySelector(".image-container")
-const startButton = document.querySelector(".start-button")
-const gameText = document.querySelector(".game-text")
-const playTime = document.querySelector(".play-time")

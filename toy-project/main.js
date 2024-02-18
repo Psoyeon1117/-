8a4eae2801cee55e05dbcce56470f7b7a246e5ce
Psoyeon <128681094+Psoyeon1117@users.noreply.github.com>
2024-02-18 19:07:41 +0900

    Add files via upload

--- toy-project/main.js
+++ toy-project/main.js
@@ -1,34 +1,54 @@
-//2. 노트 드래그하여 이동시키기
-//3. 기본 노트 기능
-//4. 노트 검색
-//5. 노트 삭제
-//6. 회원가입, 로그인
+//5. 회원가입, 로그인
+//6. 테마 기능 + 화면 구성 선택
+import * as note from "./note.js";
+
+const noteContainer = document.querySelector(".note-container")
+const plus = document.querySelector(".plus")
+const trashCan = document.querySelector(".delete")
+const input = document.querySelector(".input")
+const title = document.getElementsByClassName("title")
+const notetiles = document.getElementsByTagName("li")
+
 
 //1. 노트 추가 기능
 //+를 누르면 맨 뒤에 노트가 추가된다.
-//필수로 제목을 추가해야한다.
-
-const mainSection = document.querySelector(".main-section > ul")
-const plus = document.querySelector(".plus")
-
 plus.addEventListener("click", () => {
-    // console.log(e)
-    noteplus()
+    note.noteplus(noteContainer)
+    console.log(title)
 })
 
-const noteplus = () => {
-    const li = document.createElement("li")
-    const title = generateItem("title")
-    const story = generateItem("story")
-    li.appendChild(title)
-    li.appendChild(story)
-    mainSection.appendChild(li)
-}
-const generateItem = (classname) => {
-    const item = document.createElement("div")
-    const randomId = Math.random().toString(36).substring(2, 12)
-    item.classList.add(classname)
-    item.setAttribute("id",randomId)
-    item.setAttribute("contenteditable","true")
-    return item
-}
+
+//2. 노트 드래그하여 이동시키기
+noteContainer.addEventListener("dragstart", (e) => {
+    const obj = e.target;
+    note.dragged.el = obj;  //note>dragged 객체에 드래그한 노트의 정보를 입력
+    note.dragged.index = [...obj.parentNode.children].indexOf(obj);
+})
+noteContainer.addEventListener("dragover", (e) => {
+    e.preventDefault()
+})
+noteContainer.addEventListener("drop", (e) => {
+    note.dropFunc(e)    //drop 방식을 정하는 함수를 호출
+})
+
+
+//3. 노트 삭제
+trashCan.addEventListener("click", (e) => {
+    noteContainer.addEventListener()
+})
+
+
+//4. 노트 검색
+input.addEventListener("keyup", (e) => {
+    if(e.keyCode === 13){
+        note.search(e.target.value)
+        console.log(title)
+    }
+})
+input.addEventListener("input", (e) => {
+    if(e.target.value == ""){
+        for(let i=0;i<title.length; i++){
+            notetiles[i].style.display = '';
+        }
+    }
+})
---
+++ toy-project/note.js
@@ -0,0 +1,56 @@
+const notetiles = document.getElementsByTagName("li")
+const title = document.getElementsByClassName("title")
+
+let dobj;
+let originPlace;
+let isLast = false;
+export const dragged = {
+    el: null,
+    index: null,
+}
+
+export const noteplus = (mainSection) => {  //note를 추가하는 함수
+    const li = document.createElement("li")
+    const title = generateItem("title")
+    const story = generateItem("story")
+    li.appendChild(title)
+    li.appendChild(story)
+    li.setAttribute("draggable", "true")
+    mainSection.appendChild(li)
+}
+const generateItem = (classname) => {   //요소를 만드는 함수
+    const item = document.createElement("div")
+    const randomId = Math.random().toString(36).substring(2, 12)
+    item.classList.add(classname)
+    item.setAttribute("id",randomId)
+    item.setAttribute("contenteditable","true")
+    return item
+}
+
+
+export const dropFunc = (e) => {
+    if (e.target ==  "li"){
+        dobj = e.target
+    }else{dobj = e.target.parentNode}
+    //drop.target이 li가 아닐 경우에 대한 if문
+    if(dragged.el.nextSibling){ //drag조각이 마지막 조각인지 확인하는 곳
+        originPlace = dragged.el.nextSibling;
+    //drag조각!=마지막 조각이라면, drag조각의 이후조각을 원 위치로 설정한다.
+    }else{
+        originPlace = dragged.el.previousSibling;
+        isLast = true
+    }
+    const dropindex = [...dobj.parentNode.children].indexOf(dobj)
+    dragged.index < dropindex?dobj.after(dragged.el):dobj.before(dragged.el)
+    //drag조각이 drop조각보다 앞에 있을 경우, drag조각이 drop조각의 뒤로 가도록 함
+    isLast ? originPlace.after(dobj) : originPlace.before(dobj);
+    //drag조각이 마지막이라면, 원 위치의 뒤에 drop조각을 둔다.
+}
+
+export const search = (words) => {      //제목을 검색하는 함수
+    for(let i=0;i<title.length; i++){
+        if(title[i].innerText != words){    //제목과 검색내용이 일치하지 않을 경우 노트를 숨긴다.
+            notetiles[i].style.display = 'none';
+        }
+    }
+}
